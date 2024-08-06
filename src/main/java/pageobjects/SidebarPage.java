671fa1597f027cc3e3187a04ecf92682f3553c5b
silviya.markova <silviya.markova@delasport.com>
2024-08-06 10:29:38 +0300

    Updated the suite paths in the testng xml files.

--- src/main/java/pageobjects/SidebarPage.java
+++ src/main/java/pageobjects/SidebarPage.java
@@ -9,20 +9,20 @@ import java.util.stream.Collectors;
 
 public class SidebarPage extends BasePage {
 
-    public SidebarPage(){
+    public SidebarPage() {
         super();
     }
 
     @FindBy(css = "nav.menu .theme-doc-sidebar-menu .menu__list-item")
     private List<WebElement> sidebarItems;
 
-    @FindBy(css=".menu__list-item-collapsible a[href*='/docs/api/protocols']")
+    @FindBy(css = ".menu__list-item-collapsible a[href*='/docs/api/protocols']")
     private WebElement protocolsSection;
 
-    @FindBy(css="li.theme-doc-sidebar-item-category .menu__list-item-collapsible .menu__caret")
+    @FindBy(css = "li.theme-doc-sidebar-item-category .menu__list-item-collapsible .menu__caret")
     private WebElement protocolsExpandButton;
 
-    @FindBy(css="li.theme-doc-sidebar-item-category:has(a[href*='/docs/api/protocols']) ul.menu__list li a")
+    @FindBy(css = "li.theme-doc-sidebar-item-category:has(a[href*='/docs/api/protocols']) ul.menu__list li a")
     private List<WebElement> protocolItems;
 
     public boolean isWordPresentInSidebar(String searchWord) {
@@ -34,25 +34,26 @@ public class SidebarPage extends BasePage {
         sidebarItems.forEach(item -> System.out.println(item.getText()));
     }
 
-    public List<String> getProtocolItems(){
+    public List<String> getProtocolItems() {
         waitForElementsToBeVisible(protocolItems);
         return protocolItems.stream()
                 .map(WebElement::getText)
                 .collect(Collectors.toList());
     }
 
-    public void clickProtocolsLink(){
+    public void clickProtocolsLink() {
         waitForElementToBeClickable(protocolsSection);
         protocolsSection.click();
     }
 
-    public void clickProtocolsExpandButton(){
+    public void clickProtocolsExpandButton() {
         waitForElementToBeClickable(protocolsExpandButton);
         protocolsSection.click();
     }
 
     public boolean isProtocolsSectionDisplayed() {
         try {
+            waitPageToLoad();
             return protocolsSection.isDisplayed();
         } catch (Exception e) {
             return false;
